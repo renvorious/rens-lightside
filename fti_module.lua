119c244f260b98f004447e3819781d1e697a2e26
renvorious <147282723+renvorious@users.noreply.github.com>
2024-07-02 13:08:33 +0200

    Create fti_module.lua

---
+++ fti_module.lua
@@ -0,0 +1,24 @@
+local FTIModule = {}
+
+-- // Private
+
+
+-- // Public
+
+function FTIModule:GetHandles(Character, Table)
+
+end
+
+function FTIModule:IsLimb(BasePart)
+
+end
+
+function FTIModule:FireTouchInterest(BasePart, Handle)
+
+end
+
+function FTIModule:Version()
+    warn("ver 1.0.0a")
+end
+
+return FTIModule
